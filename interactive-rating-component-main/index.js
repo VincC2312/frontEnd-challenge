d3c75111b8335cccb76d30aac21755e7991568eb
John Alvin <johnalvin.bonifacio@proton.me>
2022-08-27 23:47:43 +0800

    feat: add disable function for submit button

--- interactive-rating-component-main/index.js
+++ interactive-rating-component-main/index.js
@@ -5,16 +5,23 @@ const submitBtn = document.getElementById("submit");
 const overlay = document.querySelector(".overlay");
 const rateBTN = document.querySelectorAll(".numbers");
 const selectMessage = document.querySelector(".selected");
+let score, btnSubmit;
+
+const init = function () {
+  score = 0;
+  btnSubmit = true;
+  submitBtn.disabled = true;
+};
+
+init();
+
+rateBTN.forEach((rateBTN, index) => {
+  rateBTN.addEventListener("click", () => {
+    score = index + 1;
+  });
+});
 
 submitBtn.addEventListener("click", function () {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
-});
-
-rateBTN.forEach((rateBTN) => {
-  rateBTN.addEventListener("click", () => {});
-});
-
-submitBtn.addEventListener("click", function () {
-  selectMessage.textContent = "testing";
 });
